7790e222c34a264cf3aaefeff66c2310d27ba226
十八君 <109498680+eighteen-plus-eighteen@users.noreply.github.com>
2023-03-18 14:40:24 +0800

    Add files via upload

---
+++ js/basic1.js
@@ -0,0 +1,40 @@
+/*通过事件onclick改变图片*/
+/*长按图片时*/
+/*梅*/
+function down1(obj) {
+  obj.src = "https://cdn.jsdelivr.net/gh/wuyilin18/CDN/image/梅1.png";
+}
+/*松开图片时*/
+function up1(obj) {
+  obj.src = "https://cdn.jsdelivr.net/gh/wuyilin18/CDN/image/梅.png";
+}
+/*通过事件onclick改变图片*/
+/*长按图片时*/
+/*兰*/
+function down2(obj) {
+  obj.src = "https://cdn.jsdelivr.net/gh/wuyilin18/CDN/image/兰1.png";
+}
+/*松开图片时*/
+function up2(obj) {
+  obj.src = "https://cdn.jsdelivr.net/gh/wuyilin18/CDN/image/兰.png";
+}
+/*通过事件onclick改变图片*/
+/*长按图片时*/
+/*竹*/
+function down3(obj) {
+  obj.src = "https://cdn.jsdelivr.net/gh/wuyilin18/CDN/image/竹1.png";
+}
+/*松开图片时*/
+function up3(obj) {
+  obj.src = "https://cdn.jsdelivr.net/gh/wuyilin18/CDN/image/竹.png";
+}
+/*通过事件onclick改变图片*/
+/*长按图片时*/
+/*菊*/
+function down4(obj) {
+  obj.src = "https://cdn.jsdelivr.net/gh/wuyilin18/CDN/image/菊1.png";
+}
+/*松开图片时*/
+function up4(obj) {
+  obj.src = "https://cdn.jsdelivr.net/gh/wuyilin18/CDN/image/菊.png";
+}
